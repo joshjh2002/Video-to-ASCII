5888b311a31aa80985f3c8cc3545c0ebdeb42b56
yb012845 <yb012845@student.reading.ac.uk>
2022-03-16 13:22:34 +0000

    Refactored

--- sketch.js
+++ sketch.js
@@ -1,36 +1,33 @@
-const density = "Ñ@#W$9876543210?!abc;:+=-,._                        ";
-//const density = "Ñ@#W$9876543210?!abc;:+=-,._                    ";
-//const density = "       .:-i|=+%O#@";
-// const density = '        .:░▒▓█';
+const encoding = "Ñ@#W$9876543210?!abc;:+=-,._                        ";
 
-let video;
-let asciiDiv;
+let videoStream;
+let asciiStream;
 
 function setup() {
   noCanvas();
-  video = createCapture(VIDEO);
-  video.size(256, 128);
-  asciiDiv = createDiv();
+  videoStream = createCapture(VIDEO);
+  videoStream.size(256, 128);
+  asciiStream = createDiv();
 }
 
 function draw() {
-  video.loadPixels();
+  videoStream.loadPixels();
   let asciiImage = "";
-  for (let j = 0; j < video.height; j++) {
-    for (let i = 0; i < video.width; i++) {
-      const pixelIndex = (i + j * video.width) * 4;
-      const r = video.pixels[pixelIndex + 0];
-      const g = video.pixels[pixelIndex + 1];
-      const b = video.pixels[pixelIndex + 2];
+  for (let j = 0; j < videoStream.height; j++) {
+    for (let i = 0; i < videoStream.width; i++) {
+      const pixelIndex = (i + j * videoStream.width) * 4;
+      const r = videoStream.pixels[pixelIndex + 0];
+      const g = videoStream.pixels[pixelIndex + 1];
+      const b = videoStream.pixels[pixelIndex + 2];
 
-      const avg = (r + g + b) / 3;
-      const len = density.length;
-      const charIndex = floor(map(avg, 0, 255, 0, len));
-      const c = density.charAt(charIndex);
+      const brightness = (r + g + b) / 3;
+      const length = encoding.length;
+      const charIndex = floor(map(brightness, 0, 255, 0, length));
+      const c = encoding.charAt(charIndex);
       if (c == " ") asciiImage += "&nbsp;";
       else asciiImage += c;
     }
     asciiImage += "<br/>";
   }
-  asciiDiv.html(asciiImage);
+  asciiStream.html(asciiImage);
 }
